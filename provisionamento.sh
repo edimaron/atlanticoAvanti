c4c3911881d75fe041abb2b28de9988aebbdbae4
edimaron <88264893+edimaron@users.noreply.github.com>
2023-08-21 22:50:10 -0300

    sudo_provisionamento.sh

--- provisionamento.sh
+++ provisionamento.sh
@@ -1,30 +1,34 @@
 #!/bin/bash
 
-# Criação dos diretórios
-mkdir /publico
-mkdir /adm
-mkdir /ven
-mkdir /sec
+echo "Criação dos diretórios"
 
-# Criação dos grupos
-groupadd GRP_ADM
-groupadd GRP_VEN
-groupadd GRP_SEC
+sudo mkdir /publico
+sudo mkdir /adm
+sudo mkdir /ven
+sudo mkdir /sec
 
-# Criação dos usuários e atribuição aos grupos
-useradd carlos -G GRP_ADM
-useradd maria -G GRP_ADM
-useradd joao -G GRP_ADM
+echo "Criação dos grupos"
 
-useradd debora -G GRP_VEN
-useradd sebastiana -G GRP_VEN
-useradd roberto -G GRP_VEN
+sudo groupadd GRP_ADM
+sudo groupadd GRP_VEN
+sudo groupadd GRP_SEC
 
-useradd josefina -G GRP_SEC
-useradd amanda -G GRP_SEC
-useradd rogerio -G GRP_SEC
+echo "Criação dos usuários e atribuição aos grupos"
 
-# Definição das permissões
+sudo useradd carlos -c "Carlos da Silva" -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_ADM
+sudo useradd maria -c "Maria da Silva" -m -s /bin/bash -p $(openssl passwd Senha123)G GRP_ADM
+sudo useradd joao -c "João da Silva" -m -s /bin/bash -p $(openssl passwd Senha123)-G GRP_ADM
+
+useradd debora -c "Debora da Silva" -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_VEN
+useradd sebastiana -c "Sebastiana da Silva" -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_VEN
+useradd roberto -c "Roberto da Silva" -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_VEN
+
+useradd josefina -c "Josefina da Silva" -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_SEC
+useradd amanda -c "Amanda da Silva" -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_SEC
+useradd rogerio -c "Rogerio da Silva" -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_SEC
+
+echo "Definição das permissões"
+
 chown root:root /publico
 chmod 777 /publico
 
